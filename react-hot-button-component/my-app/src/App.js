e4ddb6ab6cfb2b084c05e4a9c8df21572e8cfcc4
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-07-21 03:01:58 +0000

    Completed the react-hot-button-component challenge.

---
+++ react-hot-button-component/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HotButton from './HotButton';
+import './HotButton.css';
+
+function App() {
+  return (
+    <div>
+      <HotButton />
+    </div>
+  );
+}
+
+export default App;
