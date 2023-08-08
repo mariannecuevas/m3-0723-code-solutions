4547623da9861718d0419ddfeaf9a1623d4fcd6c
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-08-08 03:08:51 +0000

    Added prev and next functions and automatic next slide after 3 secs effect

--- react-carousel-component/my-app/src/Carousel.js
+++ react-carousel-component/my-app/src/Carousel.js
@@ -1,26 +1,53 @@
-// import { useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export default function Carousel({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  let timer;
+
+  const goToPrevSlide = () => {
+    clearInterval(timer);
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNextSlide = () => {
+    clearInterval(timer);
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, [currentIndex, images.length]);
+
   return (
-    <>
-      <div className="carousel">
-        <button className="btn left">
-          <FaChevronLeft className="arrow-icon" />
-        </button>
-        <div className="carousel-container">
-          <ul className="carousel-track">
-            {images.map((img) => (
-              <li className="slide" key={img.id}>
-                <img className="current-image" src={img.src} alt={img.alt} />
-              </li>
-            ))}
-          </ul>
-        </div>
-        <button className="btn right">
-          <FaChevronRight className="arrow-icon" />
-        </button>
+    <div className="carousel">
+      <button className="btn left" onClick={goToPrevSlide}>
+        <FaChevronLeft className="icon" />
+      </button>
+      <div className="carousel-container">
+        <ul
+          className="carousel-track"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+          {images.map((img) => (
+            <li className="slide" key={img.id}>
+              <img className="current-image" src={img.src} alt={img.alt} />
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+      <button className="btn right" onClick={goToNextSlide}>
+        <FaChevronRight className="icon" />
+      </button>
+    </div>
   );
 }
