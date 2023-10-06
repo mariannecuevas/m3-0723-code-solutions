494c077c48353380f40d9fb456257580077e8bc0
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-10-06 00:46:26 +0000

    Completed the unit-tests-jest exercise.

---
+++ unit-tests-jest/numbers.js
@@ -0,0 +1,53 @@
+/**
+ * Returns a new array that contains only the even numbers in the given array.
+ */
+export function evenNumbers(numbers) {
+  return numbers.filter((n) => n % 2 === 0);
+}
+
+/**
+ * Returns a number formatted in dollars and cents.
+ * It works for both integer and fractional numbers.
+ * That is, `toDollars(12)` returns `$12.00` and
+ * `toDollars(11.341)` returns `$11.34`.
+ */
+export function toDollars(amount) {
+  return `$${amount.toFixed(2)}`;
+}
+
+/**
+ * Returns a new array of numbers where every entry has been divided by the
+ * given divisor. Does not modify the original array.
+ * @param {Array} numbers The array of numbers to be divided by the divisor.
+ * @param {number} divisor The number to divide into each element of `numbers`.
+ * @returns a new array.
+ */
+export function divideBy(numbers, divisor) {
+  if (divisor === 0) {
+    return null;
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i] / divisor;
+  }
+  return numbers;
+}
+
+/**
+ * Modifies an object by multiplying the value of each key, but only if that
+ * value is a number. Key names are arbitrary.
+ * @param {Object} obj The object to modify.
+ * @param {number} multiplier The number to multiply by.
+ * @returns the input object.
+ */
+export function multiplyBy(obj, multiplier) {
+  const result = {};
+  Object.entries(obj).forEach(([key, value]) => {
+   if (multiplier === 0) {
+     result[key] = 0;
+   } else if (typeof value === 'number') {
+     result[key] = value * multiplier;
+   }
+  });
+  return result;
+}
---
+++ unit-tests-jest/unit-tests-jest-quiz-notes.md
@@ -0,0 +1,43 @@
+# unit-tests-jest-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What are unit tests?
+Unit test is a way of testing a unit, which is the smallest piece of code that can be logically isolated in a system.
+
+- Why is it important to write unit tests?
+It is important to write unit tests to catch bugs early, facilitate code maintenance, and refactoring.  .
+
+- What code should be tested with a unit test? What code is not well suited for unit tests?
+Small units of code should be tested with unit tests, such as a function, a subroutine, a method or property, that can be isolated. The smaller the better. Codes that cross systems or codes that involves complex interactions are not well suited for unit tests.
+
+- What is Jest? What are some other popular JavaScript unit testing frameworks?
+Jest is the most popular JavaScript unit testing framework. Some other popular JavaScript unit testing frameworks are Mocha and Jasmine.
+
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```js
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
